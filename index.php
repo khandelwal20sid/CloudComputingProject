1eb5127ff72edec211e7122d6d9cb0d5f27eb0ba
Siddharth Khandelwal <58730831+khandelwal20sid@users.noreply.github.com>
2021-06-14 22:51:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siddharth Khandelwal</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <style>
+        body {
+            padding: 0;
+            margin: 0;
+        }
+
+        .container {
+            background-color: beige;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        .cont {}
+    </style>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="cont">
+            <H1>Hey There! Siddharth here...</H1>
+            <H1>Nice to see you, have a nice day :)</H1>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
